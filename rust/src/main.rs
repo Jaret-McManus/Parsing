7d4226356296bbdc12a2ddfcb2ba3ef6a2bada96
Jaret-McManus <jaretmcmanus03@gmail.com>
2026-08-01 11:22:08 -0400

    start of basic arithmetic parser

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,25 @@
 use std::env;
 use std::fs;
 
+mod tokenizer;
+use tokenizer::Token as Token;
+
+use crate::tokenizer::print_token;
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = get_args();
+
     let filename = &args[1];
-    let _string: String = fs::read_to_string(filename)
+
+    let string: String = fs::read_to_string(filename)
         .expect(&format!("Unable to open file {filename}"));
+
+    let tokens: Vec<Token> = tokenizer::tokenize(string);
+    for token in tokens {
+        print_token(token);
+    }
+}
+
+fn get_args() -> Vec<String> {
+    return env::args().collect();
 }
--- rust/src/tokenizer.rs
+++ rust/src/tokenizer.rs
@@ -1,5 +1,41 @@
 // File to take in text and turn it into tokens
+pub enum TokenType {
+    AddOp,
+    SubtractOp,
+    MultiplyOp,
+    Invalid,
+}
 
-pub fn tokenize() -> void {
+pub struct Token {
+    token_type: TokenType,
+}
 
+pub fn tokenize(input_stream: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut index: usize = 0;
+    let chars: Vec<char> = input_stream.chars().collect();
+    while index < chars.len() {
+        let ch: char = chars[index];
+        match ch {
+            '+' => tokens.push( Token { token_type: TokenType::AddOp } ),
+            '-' => tokens.push( Token { token_type: TokenType::SubtractOp } ),
+            '*' => tokens.push( Token { token_type: TokenType::MultiplyOp } ),
+            ' ' => (), // do nothing
+            _ => tokens.push( Token { token_type: TokenType::Invalid } )
+        }
+        index += 1;
+    }
+
+    return tokens;
+}
+
+pub fn print_token(token: Token) -> () {
+    let string: String;
+    match token.token_type {
+        TokenType::AddOp => string = String::from("AddOp"),
+        TokenType::MultiplyOp => string = String::from("MultiplyOp"),
+        TokenType::SubtractOp => string = String::from("SubtractOp"),
+        TokenType::Invalid => string = String::from("Invalid")
+    }
+    println!("{string}");
 }
